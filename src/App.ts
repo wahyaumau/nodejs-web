67a24772f6f0a5303ab7242251be69f58f2bdc27
Wahyu Maulana <wahyaumau@gmail.com>
2021-05-07 09:25:57 +0700

    add /hello route

--- src/App.ts
+++ src/App.ts
@@ -11,8 +11,11 @@ const main = async() => {
       LogUtil.logger.info(req.protocol + '://' + req.hostname + req.originalUrl);
       next();
     });    
-    app.get('/health', (req: Request, res: Response) => {
-      res.json({"message": "on", "status": 200});
+    app.get('', (req: Request, res: Response) => {
+      res.json({"message": "on", "status": 200});      
+    });
+    app.get('/hello', (req: Request, res: Response) => {
+      res.json({"message": "hello", "status": 200});      
     });
     app.listen(EXPRESS_CONFIG.port, () => {    
       LogUtil.logger.info(`Listening on ${EXPRESS_CONFIG.host}:${EXPRESS_CONFIG.port}`);
